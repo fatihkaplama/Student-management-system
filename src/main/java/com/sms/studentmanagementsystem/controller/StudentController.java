ab34d7e683b1aecdb8116a682040eebd7633f1e0
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-01-26 02:19:18 +0300

    Implement add student feature

--- src/main/java/com/sms/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/sms/studentmanagementsystem/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.sms.studentmanagementsystem.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -20,5 +22,18 @@ public class StudentController {
     public String listStudents(Model model){
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("students/new")
+    public String craeteStudentForm(Model model){
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    }
+
+    @PostMapping("students")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 }
