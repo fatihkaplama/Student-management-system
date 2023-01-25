751f23af8fb3d8623ee8630363032378e9282798
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-01-26 02:09:03 +0300

    Implement get all students feature

--- src/main/java/com/sms/studentmanagementsystem/service/StudentServiceImpl.java
+++ src/main/java/com/sms/studentmanagementsystem/service/StudentServiceImpl.java
@@ -1,4 +1,21 @@
 package com.sms.studentmanagementsystem.service;
 
+import com.sms.studentmanagementsystem.entity.Student;
+import com.sms.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentServiceImpl implements StudentService{
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
